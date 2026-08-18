c3df0b1b002fcff2df51594ea6124a9768734c33
John <jdkali96@gmail.com>
2026-08-18 16:31:53 +0300

    Revert page urls to test header responsiveness

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import SiteLayout from "../components/layouts/SiteLayout";
+
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Services from "../pages/Services";
 import Contact from "../pages/Contact";
 
-export default function AppRoutes() {
+function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route element={<SiteLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
     </Routes>
   );
 }
+
+export default AppRoutes;
