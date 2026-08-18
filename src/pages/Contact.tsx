0a195600cd4cb049d6f8694fcf6880ecbc4d2bf9
John <jdkali96@gmail.com>
2026-08-18 15:32:01 +0300

    First commit of the setup

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <main>
+      <h1>Get in Touch</h1>
+    </main>
+  );
+}
---
+++ src/pages/Services.tsx
@@ -0,0 +1,7 @@
+export default function Services() {
+  return (
+    <main>
+      <h1>Our Services</h1>
+    </main>
+  );
+}
