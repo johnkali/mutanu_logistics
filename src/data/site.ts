f038d7de2d9b9120be7d8f3273a12ee6c96c7c2f
John <jdkali96@gmail.com>
2026-08-18 16:18:42 +0300

    Create the first dummy site data

---
+++ src/data/site.ts
@@ -0,0 +1,37 @@
+export const siteConfig = {
+  name: "Mutanu",
+  description:
+    "Trusted freight and logistics support tailored to your business needs.",
+  url: "https://www.mutanu.com",
+
+  contact: {
+    phone: "+254 700 000 000",
+    email: "info@mutanu.com",
+    whatsapp: "254700000000",
+  },
+
+  navigation: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+  ],
+
+  social: {
+    linkedin: "#",
+    facebook: "#",
+    instagram: "#",
+  },
+};
